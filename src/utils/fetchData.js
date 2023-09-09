8b3d370d64e14771c40e6398ac3497c5ac5feb5c
Atharva <atharvak4195@gmail.com>
2023-09-09 09:35:16 +0530

    First Commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key': 'be1fe7eacbmsh9ce42a697a3a1e8p1d81dbjsneb9680c2e130',
+    },
+  };
+  
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': 'be1fe7eacbmsh9ce42a697a3a1e8p1d81dbjsneb9680c2e130',
+    },
+  };
+  
+  export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
+    return data;
+  };
